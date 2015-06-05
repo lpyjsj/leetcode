376ee95b3d2259b39be2a72336942dce5fe70692
Peppe <lpyjsj@users.noreply.github.com>
2015-06-05 09:49:13 +0800

    Update BinaryTreePostorderTravesal.java

--- BinaryTreePostorderTravesal.java
+++ BinaryTreePostorderTravesal.java
@@ -7,17 +7,19 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ //第一种方法 将后序转换为类似前序的过程，即先访问node,然后node.right,node.left,最后将结果翻转过来
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
        Stack<TreeNode> node=new Stack<TreeNode>();
-       Stack<Integer>  value=new Stack<Integer>();
+       //Stack<Integer>  value=new Stack<Integer>();
        ArrayList<Integer> list=new ArrayList<Integer>();
        if(root!=null)    
          node.push(root);
        while(!node.empty())
        {
             TreeNode treenode= node.pop();
-            value.push(treenode.val);
+           // value.push(treenode.val);
+              list.add(treenode.val);
           
            if(treenode.left!=null)
                  node.push(treenode.left);
@@ -26,8 +28,10 @@ public class Solution {
              node.push(treenode.right);
 
        }
-       while(!value.empty())     
+      /* while(!value.empty())     
          list.add(value.pop());
+      */   
+       Collections.reverse(list);
        return list;     
     }
 }
