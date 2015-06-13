fe435d927e5f899b1db74a24e300a365bd95c9f4
Seaman <lpyjsj@gmail.com>
2015-06-13 15:36:34 +0800

    new file LinkedListCycle.java  LinkedListCycleII.java

---
+++ LinkedListCycle.java
@@ -0,0 +1,41 @@
+/*Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode first=head;
+        ListNode second=head;
+        if(head==null || head.next==null)
+        return false;
+        
+        do
+        {
+         if(first.next!=null)    
+          first=first.next.next;
+         else
+          first=first.next;    
+          
+         second=second.next;
+            
+        }while(first!=second && first!=null);
+        
+        if(first==null)
+         return false;
+       else 
+         return true;
+        
+    }
+}
---
+++ LinkedListCycleII.java
@@ -0,0 +1,42 @@
+//Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode first=head;
+        ListNode second=head;
+        ListNode cycleNode=head;
+        if(head==null || head.next==null)
+        return null;
+        do
+        {
+         if(first.next!=null)    
+             first=first.next.next;
+         else
+             first=first.next;  
+         second=second.next;
+        }while(first!=second && first!=null);
+        
+       if(first==null)
+         return null;
+       else 
+       { //这里是关键
+         while(cycleNode!=first)
+          {
+             cycleNode=cycleNode.next;
+             second=first.next;
+          }
+         return cycleNode;
+       }     
+        
+    }
+}
